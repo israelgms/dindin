6c61bfb881947a034a5b2d6661bd928b7c6dd4aa
israelgms <israelgomes13@hotmail.com>
2021-10-14 16:00:33 -0300

    Criação do Footer

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,29 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <Footer />
   </div>
 </template>
 
+<script>
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: "App",
+  
+  components: {
+    NavBar, 
+    Footer,
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap');
+* {
+  padding: 0;
+  margin: 0;
+  font-family: 'Ubuntu', sans-serif;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="footer">
+        <div>
+          <img class="logo" src="../assets/dindin-logo.png" alt="" />
+        </div>
+        <div class="texto">
+          <h2 class="titulo">dindin</h2>
+          <p class="subtitulo">educação financeira</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: #419488;
+  height: 207px;
+  color: #ffffff;
+}
+
+.footer {
+    display: flex;
+}
+
+.texto, .logo {
+  margin: 53px 10px 0 0;
+}
+
+.logo {
+    margin-left: 127px;
+}
+
+.titulo {
+  font-weight: 500;
+  font-size: 66px;
+  
+}
+
+.subtitulo {
+  font-weight: 300;
+  font-size: 19px;
+  margin-left: 8px;
+}
+</style>
