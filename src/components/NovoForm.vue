e26b351f09c74c5735a8ec37d8b8e2aa56d05b31
Nlimo <lucaswilliamsv@yahoo.com.br>
2021-10-16 21:35:08 -0300

    Att do componete NovoForm

--- src/components/NovoForm.vue
+++ src/components/NovoForm.vue
@@ -1,85 +1,157 @@
 <template>
   <div id="NovoForm">
-      <div>
-          <h3> Novo Curso</h3>
-      </div>
+    <div>
+      <h3>Novo Curso</h3>
+    </div>
     <div id="form-container">
-        <form id="novoform-form">
-            <div class="input-container">
-                <textarea name="text" id="titulo" titulo="titulo" v-model="titulo" placeholder="Titulo"></textarea>
-            </div>
-            <div class="input-container">
-                <textarea name="text" type="text" id="uploadcapa" uploadcapa="uploadcapa" v-model="uploadcapa" placeholder="upload capa"></textarea>
-            </div>
-            <div class="input-container">
-                <textarea name="text" type="text" id="professor" professor="professor" v-model="professor" placeholder="Professor"></textarea>
-            </div>  
-            <div class="input-container">
-                <textarea name="text" type="text" id="descricao" titulo="descricao" v-model="descricao" placeholder="Descrição"></textarea>
-            </div>
+      <form id="novoform-form">
+        <div class="input-container">
+          <textarea
+            name="text"
+            id="titulo"
+            titulo="titulo"
+            v-model="titulo"
+            placeholder="Titulo"
+          ></textarea>
+        </div>
+        <div class="input-container">
+          <textarea
+            name="text"
+            type="text"
+            id="uploadcapa"
+            uploadcapa="uploadcapa"
+            v-model="uploadcapa"
+            placeholder="Upload capa"
+          ></textarea>
+        </div>
+        <div class="input-container">
+          <textarea
+            name="text"
+            type="text"
+            id="professor"
+            professor="professor"
+            v-model="professor"
+            placeholder="Professor"
+          ></textarea>
+        </div>
+        <div class="input-container">
+          <textarea
+            name="text"
+            type="text"
+            id="descricao"
+            titulo="descricao"
+            v-model="descricao"
+            placeholder="Descrição"
+          ></textarea>
+        </div>
 
-            <div class="input-container">
-                <textarea name="text" type="text" id="tituloaula" titulo="tituloaula" v-model="tituloaula" placeholder="Titulo da aula"></textarea>
-            </div>
-            <div class="input-container">
-                <textarea name="text" type="text" id="linkaula" titulo="linkaula" v-model="linkaula" placeholder="Link da aula"></textarea>
-            </div>
-            <div class="input-container">
-                <textarea name="text" type="text" id="descricaoaula" titulo="descricaoauala" v-model="descricaoaula" placeholder="Descriçao da aula"></textarea>
-            </div>
-            <div class="input-container">
-                <input type="submit" id="submit-btn" value="Salvar">
-            </div>
-        </form>
+        <div class="form-container-aula">
+          <div id="form-container-aula-numero">
+            <p>Aula 1 (automatizar número)</p>
+          </div>
+
+          <div class="input-container-copor">
+            <textarea
+              name="text"
+              type="text"
+              id="tituloaula"
+              titulo="tituloaula"
+              v-model="tituloaula"
+              placeholder="Titulo da aula"
+            ></textarea>
+          </div>
+          <div class="input-container-copor">
+            <textarea
+              name="text"
+              type="text"
+              id="linkaula"
+              titulo="linkaula"
+              v-model="linkaula"
+              placeholder="Link da aula"
+            ></textarea>
+          </div>
+          <div class="input-container-copor">
+            <textarea
+              name="text"
+              type="text"
+              id="descricaoaula"
+              titulo="descricaoauala"
+              v-model="descricaoaula"
+              placeholder="Descriçao da aula"
+            ></textarea>
+          </div>
+
+          <div id="button-salvar">
+            <input type="submit" id="submit-btn" value="Salvar" />
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "NovoForm"
-}
+  name: "NovoForm",
+};
 </script>
 
 
 
 <style scoped>
-
-#NovoForm{
-    margin-top: 97px ;
-    margin-left: 82px;
+#NovoForm {
+  margin-top: 97px;
+  margin-left: 82px;
 }
 
-#form-container{
-    margin-top: 66px;
+#form-container {
+  margin-top: 66px;
 }
 
-.input-container{
-    margin-top: 14px;
+.input-container {
+  margin-top: 14px;
 }
 
-#descricao{
-    padding-block-end: 121px;
-    height: 150px;
-} 
-
-#descricaoaula{
-    padding-block-end: 121px;
-    height: 150px;
-     
+#form-container-aula-numero {
+  margin-top: 21px;
+  margin-bottom: 16px;
 }
 
-textarea{
-    font-size: 18px;
-    font-style: normal;
-    font-weight: normal;
-    width: 811px;
-    height: 40px;
-    background-color: #FFFFFF;
-    border: 1px solid #AEAEAE;
-    word-break: break-all;
-    box-sizing: border-box;
-    padding-inline-start: 10px;
+.input-container-copor {
+  margin-bottom: 30px;
+}
+
+#button-salvar {
+  margin-top: 43px;
+  margin-bottom: 163px;
+}
+
+#descricao {
+  padding-block-end: 121px;
+  height: 150px;
+}
+
+#descricaoaula {
+  padding-block-end: 121px;
+  height: 150px;
+}
+
+textarea {
+  font-size: 18px;
+  font-style: normal;
+  font-weight: normal;
+  width: 811px;
+  height: 40px;
+  background-color: #ffffff;
+  border: 1px solid #aeaeae;
+  word-break: break-all;
+  box-sizing: border-box;
+  padding-inline-start: 10px;
+  padding-top: 10px;
+}
+
+textarea[placeholder] {
+  padding: auto;
 }
 
 /* input[placeholder="Descrição"]{
@@ -92,5 +164,4 @@ input[placeholder="Descrição da aula"]{
     padding-block-end: 121px;
     height: 150px;
 } */
-
 </style>
